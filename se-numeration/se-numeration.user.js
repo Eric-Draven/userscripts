efa0094bb4e96bd84001b7d15f3bfff84a19e2ba
Eric Draven <Eric-Draven@users.noreply.github.com>
2017-08-06 15:33:16 +0300

    Update se-numeration.user.js

--- se-numeration/se-numeration.user.js
+++ se-numeration/se-numeration.user.js
@@ -2,7 +2,7 @@
 // @name Numeration for Search Engines
 // @namespace se-numeration
 // @description Нумерация для поисковиков: Yandex, Google, Mail.ru, Rambler, Yahoo, Bing, Sputnik. Полезен исключительно для пользователей системы продвижения сайтов - userator.ru
-// @version 1.3.1
+// @version 1.3.2
 // @author Eric Draven
 // @updateURL https://github.com/Eric-Draven/userscripts/raw/master/se-numeration/se-numeration.meta.js
 // @downloadURL https://github.com/Eric-Draven/userscripts/raw/master/se-numeration/se-numeration.user.js
@@ -355,7 +355,7 @@
 		window.blocks = function () {
 			[].forEach.call(document.querySelectorAll('#center_col'), function (e) {
 				if (e.querySelectorAll('.se-numeration').length === 0) {
-					block = e.querySelectorAll('.g.mnr-c.g-blk, #imagebox_bigimages, .g .mod');
+					block = e.querySelectorAll('.g.mnr-c.g-blk, #imagebox_bigimages, .g.mod');
 					for (i = 0; i < block.length; i++) {
 						block[i].parentNode.removeChild(block[i]);
 					}
