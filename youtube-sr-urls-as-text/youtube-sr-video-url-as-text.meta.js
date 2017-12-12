7b13503ac7cc7e0b35f67a3f5cd96286bf2757a9
Eric Draven <Eric-Draven@users.noreply.github.com>
2017-12-12 13:16:43 +0300

    Update youtube-sr-video-url-as-text.meta.js

--- youtube-sr-urls-as-text/youtube-sr-video-url-as-text.meta.js
+++ youtube-sr-urls-as-text/youtube-sr-video-url-as-text.meta.js
@@ -2,13 +2,10 @@
 // @name YouTube Search Results Video URL as Text
 // @namespace youtube-sr-video-url-as-text
 // @description Дополнение поисковой выдачи YouTube ссылкой (в виде текста) на видео ролик. Полезен исключительно для пользователей системы продвижения сайтов - userator.ru
-// @version 1.0.1
+// @version 1.0.2
 // @author Eric Draven
 // @updateURL https://github.com/Eric-Draven/userscripts/raw/master/youtube-sr-urls-as-text/youtube-sr-video-url-as-text.meta.js
 // @downloadURL https://github.com/Eric-Draven/userscripts/raw/master/youtube-sr-urls-as-text/youtube-sr-video-url-as-text.user.js
 // @homepageURL https://github.com/Eric-Draven/userscripts/tree/master/youtube-sr-urls-as-text
 // @require https://code.jquery.com/jquery-3.2.1.slim.min.js
-// @include https://www.youtube.com/*
-// @match https://www.youtube.com/*
-// @grant GM_addStyle
 // ==/UserScript==
