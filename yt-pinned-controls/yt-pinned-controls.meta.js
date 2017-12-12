3373c3fa0032843804d0026b34b654142f601d0b
Eric Draven <Eric-Draven@users.noreply.github.com>
2017-12-12 13:21:36 +0300

    Update yt-pinned-controls.meta.js

--- yt-pinned-controls/yt-pinned-controls.meta.js
+++ yt-pinned-controls/yt-pinned-controls.meta.js
@@ -2,12 +2,9 @@
 // @name YouTube Pinned Controls
 // @namespace yt-pinned-controls
 // @description Отключение автоматического скрытия контрольной панели просматриваемого ролика на YouTube. Полезен исключительно для пользователей системы продвижения сайтов - userator.ru
-// @version 1.0
+// @version 1.0.1
 // @author Eric Draven
 // @updateURL https://github.com/Eric-Draven/userscripts/raw/master/yt-pinned-controls/yt-pinned-controls.meta.js
 // @downloadURL https://github.com/Eric-Draven/userscripts/raw/master/yt-pinned-controls/yt-pinned-controls.user.js
 // @homepageURL https://github.com/Eric-Draven/userscripts/tree/master/yt-pinned-controls
-// @include https://www.youtube.com/*
-// @match https://www.youtube.com/*
-// @grant GM_addStyle
 // ==/UserScript==
