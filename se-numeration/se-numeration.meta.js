fbab38ae7fbf9908f196a24d47eb0c33545b7c6e
Eric Draven <Eric-Draven@users.noreply.github.com>
2018-03-31 22:30:38 +0300

    Update se-numeration.meta.js

--- se-numeration/se-numeration.meta.js
+++ se-numeration/se-numeration.meta.js
@@ -2,7 +2,7 @@
 // @name Numeration for Search Engines
 // @namespace se-numeration
 // @description Нумерация для поисковиков: Yandex, Google, Mail.ru, Rambler, Yahoo, Bing, Sputnik. Полезен исключительно для пользователей системы продвижения сайтов - userator.ru
-// @version 1.5a2
+// @version 1.5a3
 // @author Eric Draven
 // @updateURL https://github.com/Eric-Draven/userscripts/raw/master/se-numeration/se-numeration.meta.js
 // @downloadURL https://github.com/Eric-Draven/userscripts/raw/master/se-numeration/se-numeration.user.js
