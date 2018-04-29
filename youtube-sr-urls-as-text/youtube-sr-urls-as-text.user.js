e500fa91f21b1ab8a2946abd55c2329a22147780
Eric Draven <Eric-Draven@users.noreply.github.com>
2018-04-29 23:26:25 +0300

    Update youtube-sr-urls-as-text.user.js

--- youtube-sr-urls-as-text/youtube-sr-urls-as-text.user.js
+++ youtube-sr-urls-as-text/youtube-sr-urls-as-text.user.js
@@ -2,12 +2,12 @@
 // @name YouTube Search Results URLs as Text
 // @namespace youtube-sr-urls-as-text
 // @description Дополнение поисковой выдачи YouTube ссылками (в виде текста) на: видео, плейлисты, каналы, пользователей. Полезен исключительно для пользователей системы продвижения сайтов - userator.ru
-// @version 1.0.4
+// @version 1.0.5
 // @author Eric Draven
 // @updateURL https://github.com/Eric-Draven/userscripts/raw/master/youtube-sr-urls-as-text/youtube-sr-urls-as-text.meta.js
 // @downloadURL https://github.com/Eric-Draven/userscripts/raw/master/youtube-sr-urls-as-text/youtube-sr-urls-as-text.user.js
 // @homepageURL https://github.com/Eric-Draven/userscripts/tree/master/youtube-sr-urls-as-text
-// @require https://code.jquery.com/jquery-3.2.1.slim.min.js
+// @require https://code.jquery.com/jquery-3.3.1.slim.min.js
 // @include https://www.youtube.com/*
 // @match https://www.youtube.com/*
 // @grant none
@@ -15,21 +15,6 @@
 
 (function () {
 	'use strict';
-
-	window.readyHandlers = [];
-	window.ready = function ready(handler) {
-		window.readyHandlers.push(handler);
-		handleState();
-	};
-	window.handleState = function handleState() {
-		if (['interactive', 'complete'].indexOf(document.readyState) > -1) {
-			while (window.readyHandlers.length > 0) {
-				(window.readyHandlers.shift())();
-			}
-		}
-	};
-	document.onreadystatechange = window.handleState;
-
 	let youtubeURL = window.location.origin,
 		headerVIDEO = '<span class="script-video">видео &#10549;</span>',
 		headerPLAYLIST = '<span class="script-playlist">плейлист &#10549;</span>',
@@ -51,20 +36,7 @@
 		return null;
 	}
 
-	GM_addStyle("#results .yt-lockup-tile {padding:10px 15px 20px 15px !important;}" +
-				".yt-lockup-movie-vertical-poster {height:" + (posterH) + "px !important;}" +
-				".yt-lockup-movie-vertical-poster .yt-lockup-movie-top-content {height:90% !important;}" +
-				".yt-lockup-movie-vertical-poster .yt-lockup-movie-bottom-content {height:10% !important;}" +
-				".script-border {margin-top:7px;padding-top:7px;border-top:1px dashed #444;color:#444;font-size:13px;}" +
-				".script-video, .script-playlist, .script-channel, .script-user {padding-top:3px;font-size:13px;}" +
-				".script-video {color:#ca0000;}" +
-				".script-playlist {color:#9f8c00;}" +
-				".script-channel {color:#4f9f00;}" +
-				".script-user {color:#3a0075;}" +
-				".script-linebreak {margin-bottom:3px;}" +
-				".script-mb {margin-bottom:10px;}");
-
-	window.blocks = function () {
+	function yt_loop() {
 		if (window.location.href.indexOf("/results?") >= 0) {
 			linkto = 'Ссылка на: ';
 			linksto = 'Ссылки на: ';
@@ -131,7 +103,19 @@
 				}
 			});
 		}
-	};
-	window.addEventListener('DOMNodeInserted', blocks, false);
-	blocks();
+	}
+
+	GM_addStyle("#results .yt-lockup-tile {padding:10px 15px 20px 15px !important;}" +
+				".yt-lockup-movie-vertical-poster {height:" + (posterH) + "px !important;}" +
+				".yt-lockup-movie-vertical-poster .yt-lockup-movie-top-content {height:90% !important;}" +
+				".yt-lockup-movie-vertical-poster .yt-lockup-movie-bottom-content {height:10% !important;}" +
+				".script-border {margin-top:7px;padding-top:7px;border-top:1px dashed #444;color:#444;font-size:13px;}" +
+				".script-video, .script-playlist, .script-channel, .script-user {padding-top:3px;font-size:13px;}" +
+				".script-video {color:#ca0000;}" +
+				".script-playlist {color:#9f8c00;}" +
+				".script-channel {color:#4f9f00;}" +
+				".script-user {color:#3a0075;}" +
+				".script-linebreak {margin-bottom:3px;}" +
+				".script-mb {margin-bottom:10px;}");
+	window.addEventListener('DOMNodeInserted', yt_loop, false);
 })();
